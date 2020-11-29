a0e9707b39fb65167fc5b0e6ebd2fa435c3414f5
papercrownking <tom_j_ellis@me.com>
2020-11-29 11:09:22 +0000

    skeleton setup
    setup of test and classes for test

--- src/pairing-test.js
+++ src/pairing-test.js
@@ -1,7 +1,30 @@
-function pairingTest() {
-    return false;
+
+class exampleClass {
+
+  constructor(param1, param2){
+    this.param1 = param1;
+    this.param2 = param2;
+  }
+
+  classFunction(){
+    return true;
+  }
 }
 
+//SET CONSTANTS HERE FOR MY TEST
+const classParamOne = false, classParamTwo = true;
+
+//INITIATE INSTANCE OF CLASS
+let newClass = new exampleClass(classParamOne, classParamTwo);
+
+//FUNCTION TO DEFINE TEST
+executeFunction = () => {
+    return newClass.classFunction();
+}
+
+executeFunction();
+
 module.exports = {
-    pairingTest
+    exampleClass,
+    executeFunction
 };
