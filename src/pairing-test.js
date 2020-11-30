0c8b1276ce6c49987c239a3cf129152e2baa9e03
papercrownking <tom_j_ellis@me.com>
2020-11-30 12:17:16 +0000

    Committing the test
    test

--- src/pairing-test.js
+++ src/pairing-test.js
@@ -1,30 +1,120 @@
+class grid {
 
-class exampleClass {
-
-  constructor(param1, param2){
-    this.param1 = param1;
-    this.param2 = param2;
+  constructor(rows, columns){
+    this.rowCount = rows;
+    this.columnCount = columns;
+    this.data = [];
+    this.robot = [];
+    this.create();
   }
 
-  classFunction(){
-    return true;
+  create(){
+    for (let i = 0; i < this.rowCount; i++) {
+      this.data[i] = [];
+      for (let j = 0; j < this.columnCount; j++) {
+        this.data[i].push(0);
+      }
+    }
   }
+
+  addRobot(row, column) {
+    if (this.robot.length > 0) {
+      return false;
+    }
+
+    this.data[row][column] = 'X';
+    this.robot = [row, column];
+  }
+
+  // fetchRobot() {
+  //   let robotCoord = [];
+
+  //   for (let i = 0; i < this.rowCount; i++) {
+  //     for (let j = 0; j < this.columnCount; j++) {
+  //       if (data[i][j] === 'X') {
+  //         return [i, j];
+  //       }
+  //     }
+  //   }     
+  // }
+  
+  //N, E, S, W
+  move(position) {
+
+    if (position === 'N') {
+      if (this.robot[0] == 0) {
+        return false;
+      } else {
+        this.data[this.robot[0]][this.robot[1]] = 0;
+
+        let newRowCord = this.robot[0]+1;
+        console.log('-----this.data---', this.data[newRowCord]);
+        this.data[newRowCord][this.robot[1]] = 'X';
+        this.robot[0] += 1;
+      } 
+    }
+
+    if (position === 'S') {
+      if (this.robot[0] == 9) {
+        return false;
+      } else {
+        this.data[this.robot[0]][this.robot[1]] = 0;
+        this.data[this.robot[0]-1][this.robot[1]] = 'X';
+        this.robot[0] -= 1;
+      } 
+    }
+
+    if (position === 'E') {
+      if (this.robot[1] == 9) {
+        return false;
+      } else {
+        this.data[this.robot[0]][this.robot[1]] = 0;
+        this.data[this.robot[0]][this.robot[1]+1] = 'X';
+        this.robot[1] += 1;
+      } 
+    }
+
+    if (position === 'W') {
+      if (this.robot[1] == 0) {
+        return false;
+      } else {
+        this.data[this.robot[0]][this.robot[1]] = 0;
+        this.data[this.robot[0]][this.robot[1]-1] = 'X';
+        this.robot[1] -= 1;
+      } 
+    }
+
+  }
+
 }
 
-//SET CONSTANTS HERE FOR MY TEST
-const classParamOne = false, classParamTwo = true;
+// //SET CONSTANTS HERE FOR MY TEST
+// const classParamOne = false, classParamTwo = true;
 
-//INITIATE INSTANCE OF CLASS
-let newClass = new exampleClass(classParamOne, classParamTwo);
+// //INITIATE INSTANCE OF CLASS
+let newWarehouse = new grid(10, 10);
+let command = 'N E N E N E N E';
 
 //FUNCTION TO DEFINE TEST
 executeFunction = () => {
-    return newClass.classFunction();
+    
+    newWarehouse.addRobot(9, 0);
+    
+    let commandArray = command.split('');
+    
+    for (let i = 0; i < commandArray.length; i++) {
+      newWarehouse.moveRobot(commandArray[i]);
+    } 
+
+    return [newWarehouse.robot[0], newWarehouse.robot[1]];
+
+    //N E S W = string
+    // return newClass.classFunction();
 }
 
-executeFunction();
+const finishPositionOfRobot = executeFunction();
+
 
 module.exports = {
-    exampleClass,
-    executeFunction
+    grid
 };
--- test/test.js
+++ test/test.js
@@ -1,32 +1,67 @@
-const { exampleClass, executeFunction } = require('../src/pairing-test');
+const { grid } = require('../src/pairing-test');
 
 
-describe("exampleClass", () => {
+describe("grid class", () => {
 
 	beforeAll(() => {
-		testClass = new exampleClass(true, false);
+		gridClass = new grid(10, 10);
 	});
 
 	test('expect parameter one to be set', () => {
-		expect(testClass.param1).toBe(true);
+		expect(gridClass.rowCount).toBe(10);
 	});
 
 	test('expect parameter two to be set', () => {
-		expect(testClass.param2).toBe(false);
+		expect(gridClass.columnCount).toBe(10);
 	});
+
+
+	test('expect grid data rows be set', () => {
+		expect(gridClass.data.length).toBe(10);
+	});
+
+
+	test('expect grid data column be set', () => {
+		expect(gridClass.data[0].length).toBe(10);
+	});
+
+
+	describe("robot functions", () => {
+		beforeAll(() => {
+			gridClass.addRobot(9, 0);
+		});
+
+
+		test('expect robot coordinates are set', () => {
+			expect(gridClass.robot[0]).toBe(9);
+			expect(gridClass.robot[1]).toBe(0);
+		});
+
+
+		test('expect robot to move north without wall', () => {
+
+			gridClass.move('N')
+
+			expect(gridClass.robot[0]).toBe(8);
+			expect(gridClass.robot[1]).toBe(0);
+		});
+
+
+	});
+
 
 });
 
 
-describe("execute Function", () => {
+// describe("execute Function", () => {
 
-	beforeAll(() => {
+// 	beforeAll(() => {
 
-	});
+// 	});
 
-	test('a passing test', () => {
-		expect(executeFunction()).toBe(true);
-	});
+// 	test('a passing test', () => {
+// 		expect(executeFunction()).toBe(true);
+// 	});
 
-});
+// });
 
